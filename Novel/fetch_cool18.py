a846853b8cc6ab03f8d3d22b6ddfe613e9749969
wangjiezhe <wangjiezhe@gmail.com>
2016-04-06 22:01:06 +0800

    Refactored
    Rewrite with OOP
    Use PyQuery instead of BeautifulSoup
    
    Signed-off-by: wangjiezhe <wangjiezhe@gmail.com>

--- Novel/fetch_cool18.py
+++ Novel/fetch_cool18.py
@@ -2,58 +2,83 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import re
 import os.path
-import requests
 from itertools import count
-from bs4 import BeautifulSoup
+from pyquery import PyQuery as pq
 
 BASEURL = 'http://www.cool18.com/bbs4/index.php?app=forum&act=threadview&tid=%s'
-ENCODING = 'gb18030'
 GOAGENT = {'http': '127.0.0.1:8087'}
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) \
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36'}
 
 
-def refine(text):
-    text = text.replace('www.6park.com', '\n')
-    text = text.replace('\r\n', '\n')
-    return text
+class Tool(object):
+
+    def __init__(self):
+        self.remove_addr = re.compile(r'<a.*?>|</a>')  # remove hyperlink
+        # replace line break tag with '\n'
+        # self.replace_line = re.compile(r'</p>')
+        # replace paragraph tag with '\n' and two full width blank
+        self.replace_para = re.compile(r'<p.*?>')
+        # replace line break with '\n'
+        self.replace_br = re.compile(r'<br\s*/*>')
+        self.remove_r = re.compile(r'&#13;|\r')  # remove '\r'
+        self.remove_ad = re.compile(r'www\.6park\.com')
+        # remove needless content at the end
+        self.remove_end = re.compile(r'<.*?bodyend.*?>.*')
+        self.remove_ot = re.compile(r'<.*?>')  # remove all other tags
+
+    def replace(self, text):
+        text = re.sub(self.remove_addr, '', text)
+        # text = re.sub(self.replace_line, '\n', text)
+        text = re.sub(self.replace_para, '\n\n', text)
+        text = re.sub(self.replace_br, '\n', text)
+        text = re.sub(self.remove_r, '', text)
+        text = re.sub(self.remove_ad, '', text)
+        text = re.sub(self.remove_end, '', text)
+        text = re.sub(self.remove_ot, '', text)
+        return text.strip()
 
 
-def get(url):
-    req = requests.get(url, headers=HEADERS, proxies=GOAGENT)
-    if req.ok:
-        source = BeautifulSoup(req.content, from_encoding=ENCODING,
-                               features="lxml")
-        title = source.title.text
-        content = source.pre.text
-        # content = '\n'.join([line.text for line in source.pre.find_all('p')])
-        text = refine(content)
-        return title, text
+class Cool18(object):
 
+    def __init__(self, baseurl, tid, headers=None, proxies=None):
+        self.baseurl = baseurl
+        self.tid = tid
+        self.url = self.baseurl % tid
+        self.headers = headers or {}
+        self.proxies = proxies or {}
+        self.tool = Tool()
+        self.doc = pq(self.url, headers=self.headers, proxies=self.proxies)
+        self.title = self._get_title()
 
-def make_filename(title):
-    filename = '%s.txt' % title
-    if os.path.exists(filename):
-        for i in count(1):
-            filename = '%s(%d).txt' % (title, i)
-            if not os.path.exists(filename):
-                break
-    return filename
+    def _get_title(self):
+        return self.doc('title').text()
 
+    def get_content(self):
+        pre = self.doc('pre').html()
+        return self.tool.replace(pre)
 
-def download(tid):
-    url = BASEURL % tid
-    try:
-        title, text = get(url)
-        print('%s : %s' % (tid, title))
-    except AttributeError:
-        print('%s : No such page!' % tid)
-        return
-    filename = make_filename(title)
-    with open(filename, 'w') as fp:
-        fp.write(text)
+    def dump(self):
+        if self.title == '':
+            print('%s : No such page!' % self.tid)
+            return
+        filename = self._get_filename()
+        content = self.get_content()
+        print('%s : %s' % (self.tid, filename))
+        with open(filename, 'w') as fp:
+            fp.write(content)
+
+    def _get_filename(self):
+        filename = '%s.txt' % self.title
+        if os.path.exists(filename):
+            for i in count(1):
+                filename = '%s(%d).txt' % (self.title, i)
+                if not os.path.exists(filename):
+                    break
+        return filename
 
 
 def main():
@@ -63,7 +88,8 @@ def main():
         print('No specific tid!')
         sys.exit(1)
     for tid in tids:
-        download(tid)
+        coo = Cool18(BASEURL, tid, HEADERS, GOAGENT)
+        coo.dump()
 
 
 if __name__ == '__main__':
